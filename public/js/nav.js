5e0fe091e2317fb1cd344541e303ebfd10d06f4f
Sarthak950 <panditsamsar950@gmail.com>
2023-07-02 18:30:32 +0530

    hey

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,1 +1,23 @@
+import * as gsap from '../../node_modules/gsap'
 
+const sound_btn = document.getElementById('sound-click-container')
+
+sound_btn.addEventListener('click', ()=>{
+
+
+    let container = document.getElementById('sound-on-off-container')
+
+
+    if(container.classList.contains('ON')){
+
+        container.style.transform = 'translateY(-50%)'
+        container.classList.remove('ON')
+        container.classList.add('OFF')
+        console.log('on')
+    }else{
+        container.style.transform = 'translateY(0%)'
+        container.classList.remove('OFF')
+        container.classList.add('ON')
+        console.log('off')
+    }
+})
