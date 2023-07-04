3124010c2dd084ab8bc628820e9a75012979e2e1
Sarthak950 <panditsamsar950@gmail.com>
2023-07-04 14:13:22 +0530

    top nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -27,3 +27,87 @@ sound_btn.addEventListener('click', () => {
     }
 
 })
+
+
+const about = document.getElementById('about')
+const work = document.getElementById('work')
+const contact = document.getElementById('contact')
+
+
+about.addEventListener('mouseover',()=>{
+
+    gsap.to('#about-rest',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(-100%)'
+    })
+    gsap.to('#about-current',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(-100%)'
+    })
+})
+//check if the mouse move away
+about.addEventListener('mouseout',()=>{
+    gsap.to('#about-rest',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(0%)'
+    })
+    gsap.to('#about-current',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(0%)'
+    })
+})
+
+work.addEventListener('mouseover', ()=>{
+        gsap.to('#work-rest',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(-100%)'
+    })
+    gsap.to('#work-current',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(-100%)'
+    })
+})
+work.addEventListener('mouseout', ()=>{
+        gsap.to('#work-rest',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(0%)'
+    })
+    gsap.to('#work-current',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(0%)'
+    })
+})
+
+contact.addEventListener('mouseover',()=>{
+ gsap.to('#contact-rest',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(-100%)'
+    })
+    gsap.to('#contact-current',{
+        duration:0.5,
+        ease:'power4.out',
+        transform:'translateY(-100%)'
+    })
+})
+
+contact.addEventListener('mouseout',()=>{
+    gsap.to('#contact-rest',{
+            duration:0.5,
+            ease:'power4.out',
+            transform:'translateY(0%)'
+        })
+        gsap.to('#contact-current',{
+            duration:0.5,
+            ease:'power4.out',
+            transform:'translateY(0%)'
+        })
+})
