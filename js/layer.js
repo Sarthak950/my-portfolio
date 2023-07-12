f88f6278150d78d7730a538e10a6b7013a3ea371
Sarthak950 <panditsamsar950@gmail.com>
2023-07-13 00:47:28 +0530

    finalixed the curson and solved the decade old problem of total website mouse movement

--- js/layer.js
+++ js/layer.js
@@ -2,27 +2,60 @@ import {gsap } from 'gsap'
 
 
 const layer = document.getElementById('bottom-layer')
-const mouse_cursor = document.getElementById('mouse')
 
+let x = 0;
+let y = 0;
+let scroll= 0 ;
+
+
+let height = Math.max(
+    document.documentElement.clientHeight,
+    document.body.clientHeight
+)
+
+
+ 
 window.addEventListener('mousemove', (e)=>{
-    const { clientX, clientY} = e;
-    const x = Math.round((clientX / window.innerWidth) * 100);
-    const y = Math.round((clientY / window.innerHeight) * 100);
 
+
+    let scrollLeft =  document.documentElement.scrollLeft;
+    let scrollTop = document.documentElement.scrollTop;
+
+    x = Math.round((e.clientX + scrollLeft) / window.innerWidth * 100);  
+    y = Math.round((e.clientY + scrollTop) / height * 100);
+
+    // gsap.to(layer,{
+    //     '--x': `${x}%`,
+    //     '--y': `${y}%`,
+    //     duration:0.5,
+    //     ease: 'sine.out'
+    // })
+
+    moveLayer(x,y)
+
+})
+
+
+window.addEventListener('scroll', (e)=>{
+    let scrollLeft =  document.documentElement.scrollLeft;
+    let scrollTop = document.documentElement.scrollTop;
+
+   
+    y += Math.round((scrollTop - scroll) / height * 100);
+    scroll = scrollTop;  
+
+    moveLayer(x,y)
+
+})
+
+
+const moveLayer = (x, y)=>{
     gsap.to(layer,{
         '--x': `${x}%`,
         '--y': `${y}%`,
         duration:0.5,
         ease: 'sine.out'
     })
-       //make the changes in the style in the js file 
-    gsap.to(mouse_cursor,{
-        '--x': `${x}%`,
-        '--y': `${y}%`,
-        duration:0.5,
-        ease: 'sine.out'
-    })
-})
-
+}
 
 
