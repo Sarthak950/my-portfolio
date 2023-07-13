5536e1f65b81fa25362e4eef8c87c2754182ce9f
Sarthak950 <panditsamsar950@gmail.com>
2023-07-13 17:11:36 +0530

    loco scrolll start

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    multiplier: 0.1,
-    //make it have a littile bit of inertia
-    inertia: 0.5
-});
+// import LocomotiveScroll from 'locomotive-scroll';
+//
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true,
+//     multiplier: 1,
+//     //make it have a littile bit of inertia
+//     inertia: 0.5
+// });
