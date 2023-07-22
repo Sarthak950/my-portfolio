5edb900e96d2ae245aae486bccc9858c6aeaee7c
Sarthak950 <panditsamsar950@gmail.com>
2023-07-22 21:14:50 +0530

    starting the footer

--- js/about_me.js
+++ js/about_me.js
@@ -11,7 +11,7 @@ ScrollTrigger.defaults({
 var scroll = new LocomotiveScroll( {
     el: document.querySelector( '[data-scroll-container]' ),
     smooth: true,
-    multiplier: .3,
+    multiplier: 2.9,
     getDirection: true,
     momentum: true,
 });
